e9704a5f05752b52b108e71ed58844a4b7b77b90
ivantalko <108145743+ivantalko@users.noreply.github.com>
2022-09-09 17:03:51 +0300

    2312

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+// находим div куда будем записывать галлерею
+const gallaryEL = document.querySelector(".gallery");
+console.log(gallaryEL)
+// создаём переменную для функции которя перебирает и возвращает масив строк.
+const markup = createMarkup(galleryItems)
+// добавляем в разметку строку(с галереей)
+// console.log(markup)
+gallaryEL.insertAdjacentHTML("beforeend", markup) 
+
+// создём функцию c параметром items для перебора обьекта с фотографиями и значениями.
+function createMarkup(items) {
+return items.map(item => {
+return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+ }).join("");              
+                
+};
