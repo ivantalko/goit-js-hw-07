f9d301e9febb0fe853f41e822e6519678b9893e4
ivantalko <108145743+ivantalko@users.noreply.github.com>
2022-09-09 18:54:21 +0300

    home work 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 // console.log(galleryItems);
 // находим div куда будем записывать галлерею
 const gallaryEL = document.querySelector(".gallery");
-console.log(gallaryEL)
+console.log(gallaryEL);
 // создаём переменную для функции которя перебирает и возвращает масив строк.
-const markup = createMarkup(galleryItems)
+const markup = createMarkup(galleryItems);
 // добавляем в разметку строку(с галереей)
 // console.log(markup)
-gallaryEL.insertAdjacentHTML("beforeend", markup) 
+gallaryEL.insertAdjacentHTML("beforeend", markup);
+//  вешаем слушателя событий на галерею и добавляем коллбэк функции.
+gallaryEL.addEventListener("click", getImgsrc);
+// создаём функцию для получения data-set(дата-атрибута)
+function getImgsrc(event) {
+  // остановка дефолтное поведение браузера(перезагрузка страницы.)
+  event.preventDefault();
+  if (event.target.tagName !== "IMG") return;
+  const dataGalley = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<div class="modal">
+      <img src="${dataGalley}" alt="cartoons" width="800"/>
+    </div>
+`
+  );
+
+  instance.show();
+}
 
 // создём функцию c параметром items для перебора обьекта с фотографиями и значениями.
 function createMarkup(items) {
-return items.map(item => {
-return `<div class="gallery__item">
+  return items
+    .map((item) => {
+      return `<div class="gallery__item">
   <a class="gallery__link" href="${item.original}">
     <img
       class="gallery__image"
@@ -24,6 +42,6 @@ return `<div class="gallery__item">
     />
   </a>
 </div>`;
- }).join("");              
-                
-};
+    })
+    .join("");
+}
